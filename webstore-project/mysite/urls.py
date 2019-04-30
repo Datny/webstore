447263a7458d73bc447eeeac344a751715b64675
datny <robert.przydatek.cl@gmail.com>
2019-04-30 11:43:54 +0200

    create view with slug

--- webstore-project/mysite/urls.py
+++ webstore-project/mysite/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.home, name="home")
+    path('', views.allProdCat, name="allProdCat"),
+    path('<slug:c_slug/>', views.allProdCat(name='products_by_category')),
+
 
 
 ]
--- webstore-project/mysite/views.py
+++ webstore-project/mysite/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
 
 def home(request):
     return render(request, "mysite/home.html")
 
+#Cat view
+
+def allProdCat(request,c_slug=None):
+    c_page = None
+    product = None
+    if c_page!=None:
+        c_page = get_object_or_404(Category,slug=c_slug)
+        products = Product.objects.filter(category=c_page,available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request,'mysite/category.html', {'category': c_page, 'products': products})
